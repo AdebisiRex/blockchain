55d083fa290070767c3be1a73b375981ac26d01a
Adesina Adebisi <adeboysina@gmail.com>
2023-06-04 14:14:34 +0100

    email added

--- controller/controller.js
+++ controller/controller.js
@@ -3,7 +3,7 @@ const asyncWrapper = require("../middleware/async")
  
 const create = asyncWrapper(async (req, res) => {
     const key = await Key.create(req.body)
-    await data.save()
+    await key.save()
     res.status(201).json({key})
 })
 
